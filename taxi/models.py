4f6c613bcbc079d451d6229459c52f255ba97d50
Victor Matveyshin <vic_m@ukr.net>
2026-08-31 23:18:06 +0300

    try3

--- taxi/models.py
+++ taxi/models.py
@@ -5,12 +5,20 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
     country = models.CharField(max_length=63)
+    class Meta:
+        ordering = ("name", )
 
     def __str__(self):
         return f"{self.name} ({self.country})"
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
