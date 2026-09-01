82cae393e36b18076ae28c8c95119493dbb3d4a7
Victor Matveyshin <vic_m@ukr.net>
2026-09-01 22:46:30 +0300

    try6

--- taxi/models.py
+++ taxi/models.py
@@ -23,7 +23,7 @@ class Driver(AbstractUser):
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='cars')
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
     driver = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     class Meta:
